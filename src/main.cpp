de212d8f187393eac51b03a6c5b32d63dabd9be3
Zani Felipe <62755729+FelipeZani@users.noreply.github.com>
2023-08-18 00:45:43 -0300

    space between segments of the body
    now the snake has a space separeting each segment of the body between them

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,6 @@
 #include <vector>
 #include <deque>
 #include <string>
-#include <cmath>
 #include "snake.hpp"
 
 const int nb_apples = 100;
@@ -31,12 +30,12 @@ int main( int argc, char * argv [] )
     getError(renderer);
     
     //player setup 
-    player p_snake = player("sprites/headSpriteSheet.png", "sprites/bodypixel.png",renderer,1,4);
+    player p_snake = player("sprites/headSpriteSheet1.png", "sprites/bodypixel.png",renderer,1,4);
     
     //snake body
     std::deque <SDL_Rect> rq;
     int size = 1;
-    int segmentIndex = 0; //segment to render
+    int prev_dir = LEFT;
     // apple container
     std::vector <SDL_Rect> apple;
 
@@ -50,10 +49,10 @@ int main( int argc, char * argv [] )
     int frame_width = p_snake.headIMG->w / nb_frames;
     int frame_height = p_snake.headIMG->h;
     int sprite_frame = 0;
-    
-    
-    bool Running = true;
+    int segmentIndex;
     int dir = 0;
+
+    bool Running = true;
     
     SDL_RenderCopy(renderer, p_snake.head_texture, &p_snake.srcrect_head, &p_snake.dsrect_head);
     SDL_RenderPresent(renderer);
@@ -72,6 +71,15 @@ int main( int argc, char * argv [] )
                 if(ev.key.keysym.sym == SDLK_UP){dir = UP;}
             }
         }
+        for(int i = 0; i<size-1; i++)
+        {
+            
+            if (dir == DOWN){rq[i].y -= 1;}
+            if (dir == UP ){rq[i].y += 1;}
+            if (dir == LEFT) {rq[i].x += 1;}
+            if (dir == RIGHT){rq[i].x -= 1;}
+            if(prev_dir != dir){SDL_Delay(10); prev_dir = dir;}
+        }
         //move
         switch (dir)
         {
@@ -80,6 +88,7 @@ int main( int argc, char * argv [] )
             case LEFT: p_snake.dsrect_head.x -=  10;break;
             case RIGHT: p_snake.dsrect_head.x += 10;break;
         }
+
         // Update the sprite frame based on the direction
         switch (dir)
         {
@@ -102,12 +111,35 @@ int main( int argc, char * argv [] )
 
         break;
         }
-        // Clear the renderer
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_SetRenderDrawColor( renderer, 255, 255, 255, 255 );
+        if(rq.size()<1){rq.push_front(p_snake.dsrect_head);}
+        for(const SDL_Rect & body_segment : rq){SDL_RenderCopy(renderer,p_snake.body_texture,&p_snake.srcrect_head,&body_segment);}
+        //collision detection with apple
+        std::for_each(apple.begin(), apple.end(),[&](auto& apple)
+        {
+            float dx = apple.x-p_snake.dsrect_head.x ;
+            float dy = apple.y - p_snake.dsrect_head.y;
+            float dist = sqrt(pow(dx,2) + pow(dy,2));
+            if(dist<2) // 2 is a small number, easy to keep a good precision
+            {
+                size += 1;
+                apple.x = -10;
+                apple.y = -10;
+                std::cout<<"head: "<<p_snake.dsrect_head.x<<","<<p_snake.dsrect_head.y<<std::endl;
+                std::cout<<"body: "<<p_snake.dsbody_rect.x<<","<<p_snake.dsbody_rect.y<<std::endl;
+            }
+        });
+        
+
+        //add newest head to the snake
+        rq.push_front(p_snake.dsrect_head);
+        while(rq.size()>size){rq.pop_back();}
+        
+        //clear window
+        SDL_SetRenderDrawColor(renderer, 0,0,0,255);
         SDL_RenderClear(renderer);
-        //initialize the first head of the game
-        if(rq.size()==1){rq.push_front(p_snake.dsrect_head);}
-        // render the body
+        
+        //draw the body
         segmentIndex = 0;
         for (const SDL_Rect &body_segment : rq)
         {
@@ -121,36 +153,13 @@ int main( int argc, char * argv [] )
         }
 
         
-        //collision detection with apple
-        std::for_each(apple.begin(), apple.end(),[&](auto& apple)
-        {
-            float dx = apple.x-p_snake.dsrect_head.x ;
-            float dy = apple.y - p_snake.dsrect_head.y;
-            float dist = sqrt(pow(dx,2) + pow(dy,2));
-            if(dist<2) // 2 is a small number, easy to keep a good precision
-            {
-                size += 2;
-                apple.x = -10;
-                apple.y = -10;
-                SDL_Rect newBodySegment = rq.back();
-                rq.push_back(newBodySegment);
-            }
-        });
-        
-       
-        //clear window
-        SDL_SetRenderDrawColor(renderer, 0,0,0,255);
-        SDL_RenderClear(renderer);
-        
-        
         //Draw apples
         SDL_SetRenderDrawColor(renderer, 255,0,0,255);
         std::for_each(apple.begin(), apple.end(), [&](auto& apple)
         {
             SDL_RenderFillRect(renderer, &apple);
         });
-        
-        //SDL_RenderCopy(renderer, p_snake.head_texture, &p_snake.srcrect_head, &p_snake.dsrect_head); // Use srcrect to render only the selected sprite frame
+        //SDL_RenderCopy(renderer, p_snake.head_texture, &p_snake.srcrect_head, &p_snake.dsrect_head);  Use srcrect to render only the selected sprite frame
         SDL_RenderPresent(renderer);
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
         SDL_Delay(100);
